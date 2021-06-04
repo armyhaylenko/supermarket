ab03861b1e1a9b1fc205bb929b395e782b057706
yehorPo <66732525+yehorPo@users.noreply.github.com>
2021-06-04 16:02:40 +0300

    removed useless validation

--- frontend/js/validateUser.js
+++ frontend/js/validateUser.js
@@ -1,16 +1,16 @@
 $(function() {
-    $("#registr").on("click", validate);
+    // $("#registr").on("click", validate);
 
-    function validate() {
-        let pass = $("#password").val();
-        let conf = $("#conf").val();
-        if (pass != conf) {
-            let error = `<p class="text-danger">*Passwords are not equal</p>`
-            $("#error").append(error);
-            event.preventDefault();
-        }
+    // function validate() {
+    //     let pass = $("#password").val();
+    //     let conf = $("#conf").val();
+    //     if (pass != conf) {
+    //         let error = `<p class="text-danger">*Passwords are not equal</p>`
+    //         $("#error").append(error);
+    //         event.preventDefault();
+    //     }
 
-    }
+    // }
     $(".password").validate({
         rules: {
             password: {

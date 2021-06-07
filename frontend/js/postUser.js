8427e24634ec778a89d1dbb52d3ed37e59f06152
yehorPo <66732525+yehorPo@users.noreply.github.com>
2021-06-08 00:17:07 +0300

    change add user

--- frontend/js/postUser.js
+++ frontend/js/postUser.js
@@ -3,6 +3,10 @@ $(function() {
 
     function postUser() {
         // console.log($("#form").serialize());
-        $.post("http://localhost:3000/login", $("#form").serialize());
+        $.ajax({
+            url: "http://localhost:3000/admin/create_user",
+            headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4gRG9lIiwidXNlcl9yb2xlIjoibWFuYWdlciIsImV4cCI6MTcxNjE0Mzc3NH0.MJ430Tzz4GUyUQv3i3q_-dn92CsMSFvbqkE2_JYZ74k' },
+            data: $("#form").serialize()
+        });
     }
 })

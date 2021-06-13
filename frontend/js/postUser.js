472ab9c71e3d461b18f79faa0693ad0b24a6aded
yehorPo <66732525+yehorPo@users.noreply.github.com>
2021-06-13 17:48:37 +0300

    add fetch

--- frontend/js/postUser.js
+++ frontend/js/postUser.js
@@ -1,17 +1,31 @@
 $(function() {
     $("#registr").on('click', postUser);
 
-    function postUser() {
-        // console.log($("#form").serialize());
-        alert("1");
-        $.ajax({
-            url: "http://localhost:3000/admin/create_user",
-            method: "POST",
-            headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4gRG9lIiwidXNlcl9yb2xlIjoibWFuYWdlciIsImV4cCI6MTcxNjE0Mzc3NH0.MJ430Tzz4GUyUQv3i3q_-dn92CsMSFvbqkE2_JYZ74k',
-                'Access-Control-Allow-Origin': "*"
-            },
-            data: $("#form").serialize()
-        });
+    async function postUser() {
+
+        let response = await fetch(
+            "http://localhost:3000/admin/create_user", {
+                method: 'POST',
+                mode: 'cors',
+                credentials: 'omit',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4gRG9lIiwidXNlcl9yb2xlIjoibWFuYWdlciIsImV4cCI6MTcxNjE0Mzc3NH0.MJ430Tzz4GUyUQv3i3q_-dn92CsMSFvbqkE2_JYZ74k'
+                },
+                body: $("#form").serialize()
+            }
+
+        ).then(resp => resp.json());
+        console.log(response);
+        // fetch({
+        //     url: "http://localhost:3000/admin/create_user",
+        //     method: "POST",
+        //     type: "POST",
+        //     headers: {
+        //         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4gRG9lIiwidXNlcl9yb2xlIjoibWFuYWdlciIsImV4cCI6MTcxNjE0Mzc3NH0.MJ430Tzz4GUyUQv3i3q_-dn92CsMSFvbqkE2_JYZ74k',
+        //         'Access-Control-Allow-Origin': "*"
+        //     },
+        //     data: $("#form").serialize()
+        // });
     }
 })
